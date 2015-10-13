0b8d0a242295cefa69747ad0a73a35fcb3546465
markedwardmurray <markedwardmurray@gmail.com>
2015-10-12 21:30:53 -0400

    add example data to datastore

--- locationTrivia-dataStore/FISLocationsDataStore.m
+++ locationTrivia-dataStore/FISLocationsDataStore.m
@@ -25,8 +25,41 @@
     self = [super init];
     if (self) {
         _locations = [[NSMutableArray alloc] init];
+        [self generateSampleData];
     }
     return self;
 }
+
+- (void)generateSampleData {
+    FISLocation *location1 = [[FISLocation alloc] initWithName:@"The Empire State Building"
+                                                      latitude:40.7484
+                                                     longitude:-73.9857];
+    
+    FISTrivium *trivia1A = [[FISTrivium alloc] initWithContent:@"1,454 Feet Tall" likes:4];
+    FISTrivium *trivia1B = [[FISTrivium alloc] initWithContent:@"Cost $24,718,000 to build" likes:2];
+    
+    [location1.trivia addObjectsFromArray:@[trivia1A, trivia1B]];
+    
+    FISLocation *location2 = [[FISLocation alloc] initWithName:@"Bowling Green"
+                                                      latitude:41.3739
+                                                     longitude:-83.6508];
+    
+    FISTrivium *trivia2A = [[FISTrivium alloc] initWithContent:@"NYC's oldest park" likes:8];
+    FISTrivium *trivia2B = [[FISTrivium alloc] initWithContent:@"Made a park in 1733" likes:2];
+    FISTrivium *trivia2C = [[FISTrivium alloc] initWithContent:@"Charging Bull was created in 1989" likes:0];
+    
+    
+    [location2.trivia addObjectsFromArray:@[trivia2A, trivia2B, trivia2C]];
+    
+    FISLocation *location3 = [[FISLocation alloc] initWithName:@"Statue Of Liberty"
+                                                      latitude:40.6892
+                                                     longitude:74.0444];
+    FISTrivium *trivia3A = [[FISTrivium alloc] initWithContent:@"Gift from the french" likes:6];
+    
+    [location3.trivia addObjectsFromArray:@[trivia3A]];
+    
+    [self.locations addObjectsFromArray:@[location2, location1, location3]];
+}
+
 
 @end
