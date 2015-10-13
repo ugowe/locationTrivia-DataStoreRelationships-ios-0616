96ecb8ab6fc2101882bbae37c683765a3a5340fe
markedwardmurray <markedwardmurray@gmail.com>
2015-10-12 20:30:36 -0400

    pods: remove podfile.lock pods

--- Pods/Target Support Files/Pods-locationTrivia-dataStoreTests-Swizzlean/Pods-locationTrivia-dataStoreTests-Swizzlean-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_locationTrivia_dataStoreTests_Swizzlean : NSObject
-@end
-@implementation PodsDummy_Pods_locationTrivia_dataStoreTests_Swizzlean
-@end
